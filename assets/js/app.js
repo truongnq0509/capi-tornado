c03a99edc3ac6bd038028c15a6c4fe9263a42b20
TruongNguyen <truongmoba157@gmail.com>
2024-06-17 14:51:03 +0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -180,8 +180,54 @@
 	new Chart(ctx_6, chartData);
 
 
-
-
 }(jQuery));
+
+
+
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".step__box");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+	currentActive++;
+	if (currentActive > circles.length) {
+		currentActive = circles.length;
+	}
+	update();
+});
+
+prev.addEventListener("click", () => {
+	currentActive--;
+	if (currentActive < 1) {
+		currentActive = 1;
+	}
+	update();
+});
+
+function update() {
+	circles.forEach((circle, index) => {
+		if (index < currentActive) {
+			circle.classList.add("active");
+		} else {
+			circle.classList.remove("active");
+		}
+	});
+	const actives = document.querySelectorAll(".active");
+	progress.style.width = `${((actives.length - 1) / (circles.length - 1)) * 100
+		}%`;
+
+	if (currentActive === 1) {
+		prev.disabled = true;
+	} else if (currentActive === circles.length) {
+		next.disabled = true;
+	} else {
+		prev.disabled = false;
+		next.disabled = false;
+	}
+}
+
 
 
