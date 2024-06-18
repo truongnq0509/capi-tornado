06a43346ea8d0c849fe66ef2db5e27f9b4a7b258
TruongNguyen <truongmoba157@gmail.com>
2024-06-19 02:35:35 +0700

    1

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@
 // :: 5.0 Auth
 // :: 6.0 Step
 // :: 7.0 Slide đánh giá khách hàng
+// :: 8.0 Slide về công ty
 
 
 (function ($) {
@@ -427,35 +428,31 @@
 
 	owlCustomer.trigger('next.owl.carousel');
 
-	// :: 9.0 Newspapers
-	$(function () {
-		$('.mhn-slide').owlCarousel({
-			nav: true,
-			//loop:true,
-			slideBy: 'page',
-			rewind: false,
-			responsive: {
-				0: { items: 1 },
-				480: { items: 2 },
-				600: { items: 3 },
-				1000: { items: 4 }
+	// :: 8.0 Slide về công ty
+	var owlAboutUs = $('.owl-carousel.about__wrapper-list');
+	owlAboutUs.owlCarousel({
+		dots: false,
+		loop: true,
+		nav: false,
+		margin: 80,
+		stagePadding: 0,
+		// smartSpeed: 10000,
+		// autoplay: true,
+		// autoplayTimeout: 3000,
+		autoplayHoverPause: false,
+		slideTransition: 'linear',
+		responsive: {
+			0: {
+				items: 1
 			},
-			smartSpeed: 70,
-			onInitialized: function (e) {
-				$(e.target).find('img').each(function () {
-					if (this.complete) {
-						$(this).closest('.mhn-inner').find('.loader-circle').hide();
-						$(this).closest('.mhn-inner').find('.mhn-img').css('background-image', 'url(' + $(e.target).attr('src') + ')');
-					} else {
-						$(this).bind('load', function (e) {
-							$(e.target).closest('.mhn-inner').find('.loader-circle').hide();
-							$(e.target).closest('.mhn-inner').find('.mhn-img').css('background-image', 'url(' + $(e.target).attr('src') + ')');
-						});
-					}
-				});
+			768: {
+				items: 2
 			},
-			navText: ['<svg viewBox="0 0 24 24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>', '<svg viewBox="0 0 24 24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>']
-		});
+			992: {
+				items: 1.5
+			}
+		}
 	});
+
 }(jQuery));
 
